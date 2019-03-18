784e1586419460ca4ce9cc5080f0ee056895c458
mouse_484 <38714187+mouse484@users.noreply.github.com>
2019-03-18 15:24:26 +0900

    v.3.0.2 (#7)
    * agree
    
    * [Esc]bot3期START！！
    
    * プレイ中の修正
    
    * 開発環境用が変わった問題
    
    * ユーザー名、ID、バージョン
    
    * サーバーチャンネルユーザー数と質問とか
    
    * "緊急修正"ステータス忘れ
    
    * 鯖名、ID
    
    * サーバー情報
    
    * 色
    
    * userinfo初期設定
    
    * ユーザー情報
    
    * 役職指定なし一覧
    
    * 役職情報かんせい
    
    * 設定を本番環境用に
    
    * v.3.0.2へ変更

---
+++ commands/bot/roleinfo.js
@@ -0,0 +1,101 @@
+const { Command } = require("discord.js-commando");
+
+const permissions = {
+    ADMINISTRATOR: "管理者",
+    CREATE_INSTANT_INVITE: "招待を作成",
+    KICK_MEMBERS: "メンバーをキック",
+    BAN_MEMBERS: "メンバーをBAN",
+    MANAGE_CHANNELS: "チャンネル管理",
+    MANAGE_GUILD: "サーバー管理",
+    ADD_REACTIONS: "リアクションの追加",
+    VIEW_AUDIT_LOG: "サーバーログの表示",
+    PRIORITY_SPEAKER: "優先通話",
+    VIEW_CHANNEL: "チャンネル閲覧",
+    READ_MESSAGES: "",
+    SEND_MESSAGES: "メッセージを送信",
+    SEND_TTS_MESSAGES: "TTS メッセージを送信",
+    MANAGE_MESSAGES: "メッセージの管理",
+    EMBED_LINKS: "埋め込みリンク",
+    ATTACH_FILES: "ファイルを添付",
+    READ_MESSAGE_HISTORY: "メッセージ履歴を読む",
+    MENTION_EVERYONE: "全員宛メンション",
+    USE_EXTERNAL_EMOJIS: "外部の絵文字の使用",
+    EXTERNAL_EMOJIS: "",
+    CONNECT: "接続(通話)",
+    SPEAK: "発言(通話)",
+    MUTE_MEMBERS: "メンバーをミュート",
+    DEAFEN_MEMBERS: "メンバーのスピーカーをミュート",
+    MOVE_MEMBERS: "メンバーを移動",
+    USE_VAD: "音声検出の使用",
+    CHANGE_NICKNAME: "ニックネームの変更",
+    MANAGE_NICKNAMES: "ニックネームの管理",
+    MANAGE_ROLES: "役職の管理",
+    MANAGE_ROLES_OR_PERMISSIONS: "",
+    MANAGE_WEBHOOKS: "WebHooksの管理",
+    MANAGE_EMOJIS: "絵文字の管理",
+}
+
+module.exports = class role_info extends Command {
+    constructor(client) {
+        super(client, {
+            name: "roleinfo",
+            aliases: ["rinfo", "役職情報"],
+            group: "bot",
+            memberName: "roleinfo",
+            description: "役職の情報を表示します",
+            guildOnly: true,
+            args: [
+                {
+                    key: "role",
+                    type: "role",
+                    prompt: "役職名又はIDを入力してください",
+                    default: "",
+                },
+            ],
+        });
+    }
+
+    run(message, { role }) {
+        if (role) {
+            const r_a_p = role.serialize();
+            const role_permissions = Object.keys(r_a_p)
+                .filter(key => r_a_p[key] && permissions[key])
+                .map(key => permissions[key]).join("、  ");
+
+            const has_role = () => {
+                const has_members = `${role.members.map(m => m.user.username)}`;
+                if (has_members.length > 500) {
+                    return `${has_members.slice(0, 500)} ...`;
+                }else{
+                    return has_members;
+                }
+            }
+
+            return message.say({
+                embed: {
+                    title: `${role.name}の情報`,
+                    fields: [
+                        { name: "役職名", value: `${role}`, inline: true },
+                        { name: "ID", value: role.id, inline: true },
+                        { name: "所持人数", value: role.members.size, inline: true },
+                        { name: "権限", value: `${role_permissions}` },
+                        { name: "所持者", value: has_role() },
+                    ],
+                    color: role.color,
+                },
+            });
+        } else {
+            return message.say({
+                embed: {
+                    author: {
+                        icon_url: message.guild.iconURL,
+                        name: `${message.guild.name}の役職一覧`,
+                    },
+                    description: `${message.guild.roles.map(r => r.name).join("\n")}`,
+                    color: 0xB8E986,
+                },
+                split: true,
+            });
+        }
+    }
+};
---
+++ commands/bot/userinfo.js
@@ -0,0 +1,52 @@
+const { Command } = require("discord.js-commando");
+
+module.exports = class user_info extends Command {
+    constructor(client) {
+        super(client, {
+            name: "userinfo",
+            aliases: ["uinfo", "ユーザー情報"],
+            group: "bot",
+            memberName: "userinfo",
+            description: "ユーザーの情報を表示します",
+            args: [
+                {
+                    key: "user",
+                    type: "user",
+                    prompt: "ユーザー名又はIDを入力してください",
+                    default: "",
+                },
+            ],
+        });
+    }
+
+    run(message, { user }) {
+        const user_info = (user_id) => {
+            const user = this.client.users.get(user_id);
+            return {
+                embed: {
+                    author: {
+                        icon_url: user.avatarURL,
+                        name: `${user.username}の情報`,
+                    },
+                    thumbnail: { url: user.avatarURL },
+                    fields: [
+                        { name: "ユーザー名", value: user.tag, inline: true },
+                        { name: "ID", value: user.id, inline: true },
+                        { name: "ステータス", value: user.presence.status, inline: true },
+                        { name: "Bot", value: user.bot ? "はい" : "いいえ", inline: true },
+                    ],
+                    footer: { text: "作成日" },
+                    timestamp: user.createdAt,
+                    color: 0xB8E986,
+                },
+            };
+        }
+
+
+        if (user) {
+            return message.say(user_info(user.id));
+        } else {
+            return message.say(user_info(message.author.id));
+        }
+    }
+};
