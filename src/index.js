cca5373211fc74e12d26db38b3aa2494fbe230b4
mouse_484 <38714187+mouse484@users.noreply.github.com>
2019-09-24 21:07:01 +0900

    ✨ : Add loging (#9)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const Ecstar = require("ecstar");
 
+const events_message = require("./events/message.js");
+
 const client = new Ecstar.Client({
     prefix: "!",
+});
+
+client.on("message", message => {
+    if (!message.author.bot) events_message(client, message);
 });
 
 client.login(process.env.TOKEN);
