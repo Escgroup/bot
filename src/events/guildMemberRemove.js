93548e8a9624ea2cc2b6d7bef0f9b34391d1a7d2
mouse_484 <escmouse484@gmail.com>
2019-09-30 00:10:23 +0000

    ✨ : Add event guildMemberRemove

---
+++ src/events/guildMemberRemove.js
@@ -0,0 +1,44 @@
+const { Event } = require("ecstar");
+
+module.exports = class extends Event {
+    constructor(client) {
+        super(client, "guildMemberRemove");
+    }
+
+    run(member) {
+        const server = member.guild;
+        const user = member.user;
+        this.client.channels.get("494050319508963328").send({
+            embed: {
+                author: {
+                    name: server.name,
+                    icon_url: server.iconURL,
+                },
+                thumbnail: {
+                    url: user.avatarURL,
+                },
+                fields: [
+                    {
+                        name: "名前",
+                        value: user.username,
+                        inline: true,
+                    },
+                    {
+                        name: "ID",
+                        value: user.id,
+                        inline: true,
+                    },
+                    {
+                        name: "サーバー人数",
+                        value: server.members.size,
+                    },
+                ],
+                footer: {
+                    icon_url: user.avatarURL,
+                    text: "退出",
+                },
+                color: 0xd0021b,
+            },
+        });
+    }
+};
