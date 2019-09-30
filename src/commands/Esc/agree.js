eb971d992893002002e70c2b09ecb6dc81c35217
mouse_484 <38714187+mouse484@users.noreply.github.com>
2019-09-30 09:54:50 +0900

    🔖：更新v4.0.2 (＃13)
    * 📦 : Update Ecstar version (#11)
    
    * 📦 : Update Ecstar version
    
    * ♻️ : Fix message event
    
    * Event/guild member add (#12)
    
    * 🎉 : Add file event/guildMemberAdd,js
    
    * ✨ : Add Welcome Message & log
    
    * 🎨 : Change prefix ","
    
    * ✨ : Add event guildMemberRemove
    
    * ✨ : Add command agree
    
    * ✨ : Add user joined log
    
    * 🔖 : Update v4.0.2

---
+++ src/commands/Esc/agree.js
@@ -0,0 +1,66 @@
+const { Command } = require("ecstar");
+
+module.exports = class extends Command {
+    constructor(client) {
+        super(client, {
+            name: "agree",
+        });
+    }
+
+    run(message) {
+        if (!message.guild) return;
+
+        const role_id = "483849517071073284";
+
+        if (message.member.roles.has(role_id)) {
+            message.channel.send("認証されています。(Authenticated)");
+            return;
+        }
+
+        message.member.addRole(role_id, "認証");
+
+        this.DM(message);
+
+        message.channel.send("認証が完了しました。(Authentication completed)");
+
+        this.client.channels.get("522689755654258689").send({
+            embed: {
+                author: {
+                    name: message.author.username,
+                    icon_url: message.author.avatarURL,
+                },
+                description: `${message.author.username}さんが参加したよ 🙂
+                ${message.author.username} joined 🙂`,
+            },
+        });
+    }
+
+    DM(message) {
+        message.author.send({
+            embed: {
+                fields: [
+                    {
+                        name: "日本語",
+                        value: `認証ありがとうございます🙂
+                        <#539742654561517588>で自己紹介をしていただけるとありがたいです
+
+                        何か質問があればStaff、Assistantという役職をつけている方に気軽に聞いてください。`,
+                    },
+                    {
+                        name: "English",
+                        value: `Thank you for authentication🙂
+                        I would be grateful if you could introduce yourself <#539742654561517588>
+
+                        If you have any questions, please feel free to ask Staff or Assistant `,
+                    },
+                    {
+                        name: "自己紹介テンプレ(Self-introduction template)",
+                        value:
+                            "```\n名前:\n呼び方:\n趣味・特技:\nコメント:```" +
+                            "```\nname:\nHow to call:\nHobbies & Skills:\ncomment:```",
+                    },
+                ],
+            },
+        });
+    }
+};
