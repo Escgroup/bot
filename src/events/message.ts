b628600ee2cbda7694739d29748d03c31c0c5a5f
mouse_484 <escmouse484@gmail.com>
2020-07-20 01:13:33 +0000

    aplit messageLogger

--- src/events/message.ts
+++ src/events/message.ts
@@ -5,53 +5,18 @@ import {
   WebhookMessageOptions,
   MessageEmbed,
 } from 'discord.js';
+import { MessageLogger } from '../functions/MessageLogger';
 
 type WebhookOptions = WebhookMessageOptions & { split?: false | undefined };
 
 export = class extends Event {
+  logger = new MessageLogger(this.client);
   constructor(client: Client) {
     super(client, 'message');
   }
   run(message: Message) {
     if (message.author.bot) return;
 
-    const webhookOptions: WebhookOptions = {
-      username: message.author.tag,
-      avatarURL: message.author.avatarURL() || '',
-    };
-
-    if (message.guild) {
-      webhookOptions.embeds = [
-        new MessageEmbed()
-          .addField('User Tag', message.author.tag, true)
-          .addField('User ID', message.author.id, true)
-          .addField('Message URL', `[message URL](${message.url})`, true)
-          .addField('Message ID', message.id, true)
-          .addField('Channel name', message.channel, true)
-          .setColor(message.member?.displayColor || 0x87909c)
-          .setTimestamp(message.createdAt),
-      ];
-    } else if (message.channel.type === 'dm') {
-      webhookOptions.embeds = [
-        new MessageEmbed().setFooter('DM').setTimestamp(message.createdAt),
-      ];
-    }
-
-    if (message.attachments) {
-      webhookOptions.files = message.attachments.map(
-        (attachment) => attachment.url
-      );
-    }
-
-    this.sendLogChannel(message.cleanContent, webhookOptions);
-  }
-  async sendLogChannel(text: string, options: WebhookOptions) {
-    const logChannel = this.client.channels.cache.get(
-      this.client.config?.channel.messageLog
-    ) as TextChannel;
-
-    const webhook = (await logChannel.fetchWebhooks()).first();
-
-    webhook?.send(text, options);
+    this.logger.run(message);
   }
 };
