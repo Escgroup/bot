fafca7edb6368bbfa98bf0a3211d91fc324c54b7
mouse_484 <escmouse484@gmail.com>
2020-07-20 01:35:33 +0000

    MessageLinkViewer
    Fixes Escgroup/bot#97

--- src/events/message.ts
+++ src/events/message.ts
@@ -6,11 +6,13 @@ import {
   MessageEmbed,
 } from 'discord.js';
 import { MessageLogger } from '../functions/MessageLogger';
+import { MessageLinkViewer } from '../functions/MessageLinkViewer';
 
 type WebhookOptions = WebhookMessageOptions & { split?: false | undefined };
 
 export = class extends Event {
   logger = new MessageLogger(this.client);
+  linkViewer = new MessageLinkViewer(this.client);
   constructor(client: Client) {
     super(client, 'message');
   }
@@ -18,5 +20,6 @@ export = class extends Event {
     if (message.author.bot) return;
 
     this.logger.run(message);
+    this.linkViewer.run(message);
   }
 };
