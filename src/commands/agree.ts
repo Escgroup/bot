81462bb5fcd129bc37c7aed3953ab7746563ba7e
mouse_484 <escmouse484@gmail.com>
2021-04-28 09:53:30 +0000

    agree command

--- src/commands/agree.ts
+++ src/commands/agree.ts
@@ -1,14 +1,22 @@
 import { command } from 'ecstar';
 
-const roleID = '483849517071073284';
+const roleID = '836888226760359996';
 
 export default command(() => ({
   name: 'agree',
-  render({ send, message }) {
+  async render({ send, message }) {
     if (message.member?.roles.cache.has(roleID)) {
       return send('認証されています。(Authenticated)');
-    } else {
-      return send('no');
+    }
+
+    try {
+      await message.member?.roles.add(roleID, '認証');
+      message.channel.send('認証が完了しました。(Authentication completed)');
+    } catch (err) {
+      console.error(err);
+      send(
+        'エラーが発生しました、もう一度試してエラーが発生したらStaffに声をかけてください。'
+      );
     }
   },
 }));
