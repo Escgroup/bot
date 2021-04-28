7c8064e07add07427634e91b483ecd31b864a3b5
mouse_484 <escmouse484@gmail.com>
2021-04-28 08:52:05 +0000

    ecstar next

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,17 @@ import { Client } from 'ecstar';
 
 const client = new Client({
   prefix: ',',
-  config: {
-    channel: {
-      main: '522689755654258689',
-      messageLog: '625989592369332236',
-      readme: '638280071999913994',
-      welcome: '494423151183134730',
-      memberLog: '494050319508963328',
-    },
-    server: '443320971609374721',
-    roleId: '483849517071073284',
-  },
+  // config: {
+  //   channel: {
+  //     main: '522689755654258689',
+  //     messageLog: '625989592369332236',
+  //     readme: '638280071999913994',
+  //     welcome: '494423151183134730',
+  //     memberLog: '494050319508963328',
+  //   },
+  //   server: '443320971609374721',
+  //   roleId: '483849517071073284',
+  // },
 });
 
-client.login(process.env.TOKEN);
+client.login();
